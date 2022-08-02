dfa1a3e00f5c6b614f11ca98fa640ea42d449653
Abaulderstone <alex.baulderstone@coderacademy.edu.au>
2022-08-02 15:42:28 +1000

    changes

--- src/App.js
+++ src/App.js
@@ -7,29 +7,31 @@ import fetchBooks from './Data/FetchBooks';
 import Footer from './Components/Footer';
 
 function App() {
-	const [search, setSearch] = useState('');
-	const [books, setBooks] = useState([]);
+  const [search, setSearch] = useState('');
+  cosnt[(error, setError)] = useState('');
+  const [books, setBooks] = useState([]);
 
-	useEffect(() => {
-		const wrapper = async () => {
-			if (search === '') {
-				const data = await fetchBooks(null);
-				return setBooks(data);
-			}
-			const data = await fetchBooks(search);
-			return setBooks(data);
-		};
-		wrapper(search);
-	}, [search]);
+  useEffect(() => {
+    const wrapper = async () => {
+      try {
+        const data = await fetchBooks(search);
+        return setBooks(data);
+      } catch (e) {
+        setError(e.message);
+      }
+    };
+    wrapper(search);
+  }, [search]);
 
-	return (
-		<div className={styles.App}>
-			<Header />
-			<SearchBar onSubmit={setSearch} />
-			<BookGallery books={books} />
-			<Footer />
-		</div>
-	);
+  return (
+    <div className={styles.App}>
+      <Header />
+      <SearchBar onSubmit={setSearch} />
+      {books && <BookGallery books={books} />}
+      {error && <p>error</p>}
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
